6041de669ee377ab0f369218b9be0f566db7adb1
jono <26609800+jonomango@users.noreply.github.com>
2022-11-13 16:10:12 -0800

    Initial project setup

---
+++ examples/main.cpp
@@ -0,0 +1,7 @@
+#include <pe-builder/pe-builder.h>
+
+int main() {
+  pb::pe_builder pe;
+
+}
+
---
+++ include/pe-builder/pe-builder.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace pb {
+
+class pe_builder {
+public:
+};
+
+} // namespace pb
